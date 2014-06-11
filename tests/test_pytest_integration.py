2348bec3fed11a4fcd746059a4c63abb83674a72
Chris Rose <offby1@offby1.net>
2014-06-11 06:13:06 -0700

    Improved testing; verify shuffling

--- tests/test_pytest_integration.py
+++ tests/test_pytest_integration.py
@@ -16,3 +16,29 @@ def test_configure():
     config = mock.Mock(spec=_pytest.config.Config)
     fixture.pytest_configure(config)
     
+
+def test_modifyitems_shuffles():
+    session = mock.sentinel.Session
+    config = mock.Mock()
+    config.randomize_test_order = True
+    is_sorted = lambda l: all(l[i] <= l[i+1] for i in range(len(l)-1))
+    # try this 100 times or so
+    test_items = list(range(100))
+    for n in range(100):
+        fixture.pytest_collection_modifyitems(session, config, test_items)
+        if not is_sorted(test_items):
+            return
+    assert not is_sorted(test_items), "After 100 tries, didn't shuffle"
+
+    
+def test_modifyitems_doesnt_shuffle():
+    session = mock.sentinel.Session
+    config = mock.Mock()
+    config.randomize_test_order = False
+    is_sorted = lambda l: all(l[i] <= l[i+1] for i in range(len(l)-1))
+    # try this 100 times or so
+    test_items = list(range(100))
+    for n in range(100):
+        fixture.pytest_collection_modifyitems(session, config, test_items)
+        assert is_sorted(test_items), "The shuffle should not have happened"
+    
